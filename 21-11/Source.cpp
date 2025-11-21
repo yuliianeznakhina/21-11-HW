6a1712fc9ae2f6faf7768c06dd1e20d2f5fc5d86
neznjul46 <neznjul46@006-3>
2025-11-21 12:57:46 +0200

    Add project files.

---
+++ 21-11/Source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point() { x = rand() % 10; y = rand() % 10; }
+	Point(int _x, int _y) { x = _x; y = _y; }
+	void Print()
+	{
+		cout << "X: " << x << "\tY: " << y << endl;
+	}
+};
+
+int main()
+{
+
+	vector<Point> arr;
+	for (int i = 0; i < 10; i++)
+	{
+		arr.push_back(Point());
+	}
+
+	for ( auto ptr = arr.begin(); ptr != arr.end(); ptr++)
+	{
+		ptr->Print();
+	}
+	cout << endl;
+}
